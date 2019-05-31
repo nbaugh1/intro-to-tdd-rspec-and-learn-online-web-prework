139e2b5aa8925163cef7b8efeb1de21ff8e10af4
Nick Baughman <nbaugh1@gmail.com>
2019-05-31 06:13:17 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -2,4 +2,4 @@ def current_age_for_birth_year(birth_year, current_year)
   age_of_person = (current_year - birth_year)
 end
 
-puts current_age_for_birth_year(2003)
+puts current_age_for_birth_year(2003, 2019)
